fe0363b5d0ef6578d520c950a5a7433f4300a75c
peninah98 <78850411+peninah98@users.noreply.github.com>
2022-09-29 02:50:16 -0700

    Update 2-uniq_add.py

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-def search_replace(my_list, search, replace):
-    return [replace if i == search else i for i in my_list]
+def uniq_add(my_list=[]):
+    return sum(set(my_list)) my_list
