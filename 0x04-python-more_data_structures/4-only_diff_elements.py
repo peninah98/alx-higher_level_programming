561909c97812ccb0c7474c1bf9aa36f66dbcfde2
peninah98 <78850411+peninah98@users.noreply.github.com>
2022-09-29 03:45:12 -0700

    Update 4-only_diff_elements.py

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
-    result = [i for i in set_1 if i in set_2]
+    result = [i for i in set_1 if i not in set_2]
     return result
   
 set_1 = { "Python", "C", "Javascript" }
