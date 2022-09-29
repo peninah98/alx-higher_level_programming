ae50ab897cf10ab17fbdb70264406aa5ca616fe2
peninah98 <78850411+peninah98@users.noreply.github.com>
2022-09-29 03:12:19 -0700

    Update 3-common_elements.py

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
-  
-  common_elements = __import__('3-common_elements').common_elements
+  result = [i for i in set_1 if i in set_2]
+  return result
 
 set_1 = { "Python", "C", "Javascript" }
 set_2 = { "Bash", "C", "Ruby", "Perl" }
 c_set = common_elements(set_1, set_2)
 print(sorted(list(c_set)))
-    return set_1 & set_2
-  
